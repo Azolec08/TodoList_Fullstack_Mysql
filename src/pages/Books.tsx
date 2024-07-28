dcc6e08d0bb5dca9ed799a58f1424efb13efb8ee
Mark Celoza <markangeloceloza380@gmail.com>
2024-07-28 21:00:44 +0800

    feat:add loading and reactquery

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,38 +1,47 @@
 import { BookCard } from "@/components/BookCard";
 import { Button } from "@/components/ui/button";
 import { BooksType } from "@/types";
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import { useDebounce } from "use-debounce";
 
 const Books = () => {
-  const [books, setBooks] = useState<BooksType[]>([]);
   const [searchBook, setSearchBook] = useState<string>("");
   const [debouncedSearchBook] = useDebounce(searchBook, 500); // Debounce the search term
 
-  useEffect(() => {
-    async function data() {
-      try {
-        const result = await axios.get(
-          `https://mysql-backend-two.vercel.app/books`
-        );
-        setBooks(result.data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    data();
-  }, []);
+  const { data, error, isLoading }: UseQueryResult<BooksType[]> = useQuery({
+    queryKey: ["books"],
+    queryFn: async () => {
+      const response = await axios.get(
+        "https://mysql-backend-two.vercel.app/books"
+      );
+      return response.data;
+    },
+  });
 
   const handleSearchBookOnChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchBook(e.target.value);
   };
 
-  const Order = books.sort((a, b) => a.title.localeCompare(b.title));
-  const filteredBooks = Order.filter((data: BooksType) =>
-    data.title.toLowerCase().includes(debouncedSearchBook.toLowerCase())
+  const Order = data?.sort((a, b) => a.title.localeCompare(b.title)) || [];
+  const filteredBooks = Order.filter((book: BooksType) =>
+    book.title.toLowerCase().includes(debouncedSearchBook.toLowerCase())
   );
+
+  if (isLoading)
+    return (
+      <div className="h-[100dvh] flex items-center justify-center">
+        Loading...
+      </div>
+    );
+  if (error)
+    return (
+      <div className="h-[100dvh] flex items-center justify-center">
+        Error loading books
+      </div>
+    );
 
   return (
     <section>
@@ -48,8 +57,8 @@ const Books = () => {
           />
         </div>
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-2 w-full h-full">
-          {filteredBooks.map((data) => {
-            return <BookCard key={data.id} data={data} />;
+          {filteredBooks.map((book) => {
+            return <BookCard key={book.id} data={book} />;
           })}
         </div>
         <Link
